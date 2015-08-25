772a851ef4ca7475831bb4d2953c1029a5d2ef20
eredmiles <eanimals@comcast.net>
2015-08-25 17:00:49 -0400

    Final Commit

--- WorldBank2015/Code/data_pipeline_src/environment_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash  
-#Elissa Redmiles DSSG2015
-export PATH="/home/dssg/anaconda/bin":$PATH
-export PYTHONPATH="/home/dssg/anaconda":$PYTHONPATH
-echo $PYTHONPATH
-
-
--- WorldBank2015/Code/data_pipeline_src/full_pipeline.sh
+++ WorldBank2015/Code/data_pipeline_src/full_pipeline.sh
@@ -1,7 +1,6 @@
 #!/bin/bash  
 #Elissa Redmiles DSSG2015
-#Command outputs are located in $STDOUT_LOG
-#Error output are located in $ERROR_LOG
+#Script and Error logs are located in the log_files directory within this directory
  
 LOCALPATH='/home/dssg/dssg/Fraud-Corruption-Detection-Data-Science-Pipeline-DSSG2015'
 DATA_STORAGE='/home/dssg/dssg/Fraud-Corruption-Detection-Data-Science-Pipeline-DSSG2015/pipeline_data'
@@ -11,8 +10,8 @@ PATH_TO_SUPPLIER_FEATURE_GEN=$LOCALPATH'/WorldBank2015/Code/data_pipeline_src/su
 
 
 DATE=`date +"%m%d%Y_%H%M"`
-STDOUT_LOG='pipeline_'$DATE'.log'
-ERROR_LOG='pipeline_error_'$DATE'.log'
+STDOUT_LOG='./log_files/pipeline_'$DATE'.log'
+ERROR_LOG='./log_files/pipeline_error_'$DATE'.log'
 echo $STDOUT_LOG
 echo $ERROR_LOG
 
@@ -20,7 +19,10 @@ echo Starting DSSG2015 World Bank Complaint Ranking Pipeline
 
 #Setting Python Path - specific to allow desktop icon to run
 #create an environment setup file if necessary for setting your system PATH and PYTHONPATH variables
-bash environment_setup.sh
+###comment out if not needed
+export PATH="/home/dssg/anaconda/bin":$PATH
+export PYTHONPATH="/home/dssg/anaconda":$PYTHONPATH
+echo $PYTHONPATH
 
 echo ============================================
 echo Ranked files will be output to $DATA_STORAGE
@@ -32,19 +34,19 @@ echo Starting DSSG2015 World Bank Complaint Ranking Pipeline
 ## Contracts ##
 #Download a new set of contracts from the World Bank Website 
 echo Downloading newest contracts data set from finances.worldbank.org.
-echo ================================================================= >$STDOUT_LOG > $ERROR_LOG
-echo Downloading newest contracts data set from finances.worldbank.org. >>$STDOUT_LOG >> $ERROR_LOG
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee $STDOUT_LOG $ERROR_LOG >/dev/null
+echo Downloading newest contracts data set from finances.worldbank.org. | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 CONTRACTS_FILE=$DATA_STORAGE'/latest_contract_web_download.csv'
 wget --output-document=$CONTRACTS_FILE https://finances.worldbank.org/api/views/kdui-wcs3/rows.csv?accessType=DOWNLOAD >> $STDOUT_LOG 2>> $ERROR_LOG
 
 #Cleaning the contracts: formating dates, renaming columns, properly coding null values
 echo Cleaning contracts data set
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo Cleaning contracts data set >>$STDOUT_LOG >>$ERROR_LOG
+echo Cleaning contracts data set | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 CLEANED_CONTRACTS_FILE=$DATA_STORAGE'/latest_contract_web_download_cleaned.csv'
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/data_cleaning_script.py' -f $CONTRACTS_FILE -n '#' -r "Borrower Country,country,Total Contract Amount (USD),amount" -o $CLEANED_CONTRACTS_FILE >> $STDOUT_LOG 2>> $ERROR_LOG
@@ -52,11 +54,10 @@ python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/data_cleaning_
 #Entity Resolution Application
 echo Resolving entities in contracts data set.
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo Resolving entities in contracts data set. | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo Resolving entities in contracts data set. >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 CANONICAL_FILE=$DATA_STORAGE'/canonicalNamesV2.csv'
 ENTITY_RESOLVED_CONTRACTS_FILE=$DATA_STORAGE'/latest_contract_web_download_cleaned_resolved.csv'
@@ -65,12 +66,10 @@ python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/entity_resolut
 #Loading contract data into PostgreSQL database
 echo Loading contracts data set into database.
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo Loading contracts data set into database. | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo Loading contracts data set into database. >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 CONTRACTS_TABLE='latest_contract_web_download_cleaned_resolved'
 echo drop table \"$CONTRACTS_TABLE\"';' > droptable.sql
@@ -83,18 +82,12 @@ psql world_bank -f copydata.sql >> $STDOUT_LOG 2>> $ERROR_LOG
 
 ## Projects ##
 #Getting project data from World Bank Website which will later be joined with the contracts data
-echo Downloading newest projects data set from worldbank.org
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
-echo Loading contracts data set into database. >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
 echo Downloading newest projectts data set from finances.worldbank.org.
-echo ================================================================= >>$STDOUT_LOG >> $ERROR_LOG
-echo Downloading newest projects data set from finances.worldbank.org. >>$STDOUT_LOG >> $ERROR_LOG
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+
+echo Downloading newest projects data set from finances.worldbank.org.  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 PROJECTS_FILE=$DATA_STORAGE'/project_data.csv'
 wget http://search.worldbank.org/api/projects/all.csv -O $PROJECTS_FILE >> $STDOUT_LOG 2>> $ERROR_LOG
@@ -103,19 +96,20 @@ wget http://search.worldbank.org/api/projects/all.csv -O $PROJECTS_FILE >> $STDO
 echo Cleaning projects data set.
 
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
-echo Cleaning projects data set  >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo Cleaning projects data set   | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/data_cleaning_generic.py' -f $PROJECTS_FILE -o $PROJECTS_FILE >> $STDOUT_LOG 2>> $ERROR_LOG
 #Loading the project data to the PostgreSQL database
 echo Loading projects data set into database
 
-echo ================================================================= >>$STDOUT_LOG
-echo Loading projects data set  >>$STDOUT_LOG
-echo ================================================================= >>$STDOUT_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+
+echo Loading projects data set  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+
 
 PROJECTS_TABLE='project_data'
 echo drop table \"$PROJECTS_TABLE\"';' > droptable.sql
@@ -127,13 +121,11 @@ psql world_bank -f copydata.sql >> $STDOUT_LOG 2>> $ERROR_LOG
 
 #Joining Project data and Contracts data
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo joining project and contract data  >>$STDOUT_LOG >>$ERROR_LOG
+echo joining project and contract data  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
-
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 JOINED_TABLE='contracts_wprojects'
 echo DROP TABLE $JOINED_TABLE';'>droptable.sql
@@ -152,11 +144,11 @@ psql world_bank -f joinprojectdata.sql>> $STDOUT_LOG 2>> $ERROR_LOG
 ## Feature Generation ##
 #Generate contracts features
 echo Generating data abstractions \(features\) for at the contracts level.
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo Generating contract features  >>$STDOUT_LOG >>$ERROR_LOG
+echo Generating contract features  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 FEATURE_GEN_1_FILE=$DATA_STORAGE'/latest_contract_web_download_cleaned_feature_gen1.csv'
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/contracts_feature_gen.py' -f $ENTITY_RESOLVED_CONTRACTS_FILE -p procurement_method -wf $FEATURE_GEN_1_FILE -ppp $CURRENCY_FILE_PPP -fcrf $CURRENCY_FILE_FCRF >> $STDOUT_LOG 2>> $ERROR_LOG
@@ -164,11 +156,13 @@ python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/contracts_feat
 #Adding Labels Data Set
 ###Cleaning the investigation data
 echo Merging contracts, projects and investigations data sets. 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo labeleing data  >>$STDOUT_LOG >>$ERROR_LOG
+echo Merging contracts,projects and investigations data sets. | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo ================================================================= >>$STDOUT_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+
+
 LABELS_FILE=$DATA_STORAGE'/investigations.csv'
 echo Investigations file being used: $LABELS_FILE
 echo Last Modification Date for Investigations File:
@@ -178,68 +172,45 @@ UTF8_LABELS_FILE=$DATA_STORAGE'/utf8_investigations.csv'
 iconv -f utf-8 -t ascii -c $LABELS_FILE -o $UTF8_LABELS_FILE >> $STDOUT_LOG 2>> $ERROR_LOG
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/data_cleaning_generic.py' -f $UTF8_LABELS_FILE -o $UTF8_LABELS_FILE >> $STDOUT_LOG 2>> $ERROR_LOG
 
-###Merging Contracts W Features and Labels
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
-echo merging contracts, features labels  >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
 LABELED_CONTRACTS_FEATURE_GEN_1=$DATA_STORAGE'/labeled_contracts_cleaned_resolved_feature_gen_1.csv'
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/label.py' -c $FEATURE_GEN_1_FILE -i $UTF8_LABELS_FILE -wf $LABELED_CONTRACTS_FEATURE_GEN_1 >> $STDOUT_LOG 2>> $ERROR_LOG
 
-#importing labeled feature gen 1 file to database
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
-echo Loading labeled feature gen 1 data set  >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
 #Generating supplier features for allegations
 echo Generating supplier features for allegations data set.
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo =================================================================  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo generating supplier features for allegations  >>$STDOUT_LOG >>$ERROR_LOG
+echo generating supplier features for allegations | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 TABLE_ID_FOR_STORING_ALLEGATION_FEATURES='alleg'
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/feature_loop.py' -cf $FEATURE_GEN_1_FILE -if $LABELED_CONTRACTS_FEATURE_GEN_1 -id $TABLE_ID_FOR_STORING_ALLEGATION_FEATURES -path $PATH_TO_SUPPLIER_FEATURE_GEN >> $STDOUT_LOG 2>> $ERROR_LOG
 
 #Generating ranked list of complaints based on model.
 echo Generating ranked list of allegations.
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
-echo generating allegations ranked list  >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo generating allegations ranked list | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 TRAINING_TABLE_NAME='labeled_contracts_cleaned_resolved_feature_gen_1_alleg'
 RANKED_LIST_OF_ALLEGATIONS_FILE_NAME=$DATA_STORAGE'/prioritzed_allegations_to_investigate.csv'
 python -W ignore $LOCALPATH'/WorldBank2015/Code/modeling/predict.py' -tf $TRAINING_TABLE_NAME -fl $LOCALPATH'/WorldBank2015/Code/modeling/feature_sets_log.yaml' -train_id $TABLE_ID_FOR_STORING_ALLEGATION_FEATURES -wf $RANKED_LIST_OF_ALLEGATIONS_FILE_NAME>>$STDOUT_LOG 2>> $ERROR_LOG
 
 #Creating supplier features for contracts set
 echo Generating supplier features for contracts data set.
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo generating supplier features for contracts | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo generating supplier features for contracts  >>$STDOUT_LOG >>$ERROR_LOG
-
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
 TABLE_ID_FOR_STORING_CONTRACTS_FEATURES='cntrcts'
 python -W ignore $LOCALPATH'/WorldBank2015/Code/data_pipeline_src/feature_loop.py' -cf $FEATURE_GEN_1_FILE -if $FEATURE_GEN_1_FILE -id $TABLE_ID_FOR_STORING_CONTRACTS_FEATURES -path $PATH_TO_SUPPLIER_FEATURE_GEN >> $STDOUT_LOG 2>> $ERROR_LOG
 
 #Generating ranked list of contracts in the last N years based on model.
 echo Generating ranked list of contracts.
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
+echo generating contracts ranked lists  | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 
-echo generating contracts ranked lists  >>$STDOUT_LOG >>$ERROR_LOG
-
-echo ================================================================= >>$STDOUT_LOG >>$ERROR_LOG
-
+echo ================================================================= | tee -a $STDOUT_LOG $ERROR_LOG > /dev/null
 TRAINING_TABLE_NAME='labeled_contracts_cleaned_resolved_feature_gen_1_alleg'
 PREDICTING_TABLE_NAME='latest_contract_web_download_cleaned_feature_gen1_cntrcts'
 OUTPUT_FILE_NAME=$DATA_STORAGE'/ranked_contracts_to_investigate_for_'
